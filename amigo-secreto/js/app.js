fe35408e08c2308265bd3945b0c904e25989c498
Caue Robattini <cauegr18@gmail.com>
2025-06-16 20:34:47 -0300

    sorteio feito

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -3,7 +3,7 @@ let amigos = [];
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-    amigos.push = amigo.value;
+    amigos.push (amigo.value);
 
     if(lista.textContent == ''){
         lista.textContent = amigo.value;
@@ -14,17 +14,30 @@ function adicionar(){
 
 }
 
-function sortear(){
-embaralha(amigos);
+function sortear() {
+    embaralhar(amigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+        }
+    }
+}
     
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
 
-function embaralha(lista){
-    for(let indice = lista.lenght; indice; indice--){
 
-        const indiceAleatorio = Math.floor(Math.random() * indice);
+function reiniciar(){
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sortei').innerHTML = '';
 
-        [lista[indice -1], lista[indiceAleatorio]] =
-            [lista[indiceAleatorio], lista[indice - 1]];
-    }
 }
